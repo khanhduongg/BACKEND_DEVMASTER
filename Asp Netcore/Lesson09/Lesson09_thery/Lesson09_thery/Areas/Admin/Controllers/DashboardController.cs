253222f7d34980c662f51b1f21eab08d57870760
khanhduongg <khanhduong04.199@gmail.com>
2024-03-24 22:26:25 +0700

    Lesson08

---
+++ Asp Netcore/Lesson09/Lesson09_thery/Lesson09_thery/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lesson09_thery.Areas.Admin.Controllers
+{
+    public class DashboardController : Controller
+    {
+        [Area("Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

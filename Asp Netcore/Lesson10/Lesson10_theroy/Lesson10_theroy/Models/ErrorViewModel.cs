dc69f2d72ab58f80f365bb76c48dc13f1d07c692
khanhduongg <khanhduong04.199@gmail.com>
2024-04-09 22:15:34 +0700

    Lesson10

---
+++ Asp Netcore/Lesson10/Lesson10_theroy/Lesson10_theroy/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lesson10_theroy.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
